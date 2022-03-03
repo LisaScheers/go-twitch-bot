d66efc27c7e66972e9f070c3cdabef81d97f2598
Lisa Scheers <lisa@scheers.tech>
2022-03-03 18:01:36 +0100

    things and stuff

--- cmd/twitchbot-server/main.go
+++ cmd/twitchbot-server/main.go
@@ -2,34 +2,36 @@ package main
 
 import (
 	"github.com/LisaScheers/go-twitch-bot/pkg/bot"
+	"github.com/gempir/go-twitch-irc/v3"
 )
 
 func main() {
-	bot := bot.bot{
-		config: &bot.Config{
-			Twitch: &bot.Twitch{
-				Username: "LisaScheers",
-				Token:    "oauth:1cvh668q2689uglwcck08mv6v9oero",
-				Channel:  "LisaScheers",
-			},
-			Commands: []bot.Command{
-				{
-					Name: "command",
-					Func: func(msg bot.PrivateMessage, client *bot.Client) {
-						client.Say(msg.Channel, "test back")
-					},
-					prefix:   "!test",
-					cooldown: 10,
-				},
-			},
-			WhisperCommands: []bot.Command{
-				{
-					Name:   "whisper",
-					Func:   func(msg bot.WhisperMessage, client *bot.Client) {},
-					prefix: "!",
-				},
+	tb := bot.New(&bot.Config{
+		Twitch: bot.Twitch{
+			Username: "LisaScheers",
+			Token:    "oauth:XXxXXXX",
+			Channels: []string{"LisaScheers"},
+		},
+		Commands: []bot.Command{
+			{
+				Name:     "!timer",
+				Func:     TimerCallbackFn,
+				Prefix:   "!timer",
+				Cooldown: 5,
 			},
 		},
+		WhisperCommands: []bot.WhisperCommand{
+			{
+				Name: "!test",
+				Func: func(msg twitch.WhisperMessage, client *twitch.Client) {
+					client.Whisper(msg.User.Name, "test")
+				},
+				Prefix: "!",
+			},
+		},
+	})
+	err := tb.Setup()
+	if err != nil {
+		panic(err)
 	}
-	bot.setup()
 }
